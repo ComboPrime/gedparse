2a018573414ef69e1de379d64c5e9f94815a7e6a
ComboPrime <biz@etgdesign.com>
2013-08-27 00:07:04 -0400

    implemented page_display callback, fixed partner links and title

--- gedparse_display.tpl.php
+++ gedparse_display.tpl.php
@@ -6,22 +6,28 @@
  *
  * Available variables:
  *
- * - $title:   Used for <title> and <h1>.
- * - $gender:  A class setting to style the person's display based on gender.
- * - $html:    The rendered HTML for the selected person.
- * - $partner: The rendered HTML for the partner, if any.
+ * - $title...................Used for <title> and <h1>.
+ * - $selected_gender_class...A class setting to style the selected person's
+ *                            display based on gender.
+ * - $selected_html...........The rendered HTML for the selected person.
+ *
+ * - The following variables are available only if a partner is displayed.
+ *
+ * - $partner_gender_class....(optional) A class setting to style the partner's
+ *                            display based on gender.
+ * - $partner_html............(optional) The rendered HTML for the partner.
  */
 ?>
 
 
 <div class="row-fluid gedparse">
-<div class="span6 selected <?php print $gender; ?>">
-<?php print $html ; ?>
+<div class="span6 selected <?php print $selected_gender_class; ?>">
+<?php print $selected_html ; ?>
 </div><!-- end span6 selected person div -->
 
-<?php if ($partner): ?>
-  <div class="span6 partner">
-  <?php print $partner ; ?>
+<?php if ($partner_html): ?>
+  <div class="span6 partner <?php print $partner_gender_class; ?>">
+  <?php print $partner_html ; ?>
   </div><!-- end span6 partner div -->
   </div><!-- end row-fluid wrapper div -->
 
